17dde0aaffe8201f1ea7773f5dc24ff7f3116538
matoaster <mathew0105@gmail.com>
2025-11-08 21:52:35 +0000

    Working game clock

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from logger import log_state
 import pygame
 
 def main():
-
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,8 +14,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         screen.fill("black")
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        print(dt)
         pygame.display.flip()
 
 
